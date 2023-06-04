9fe3365b252185705556f4f1dba50b7e9b8da9a6
Andile Rodney <42125381+Andile-Rodney@users.noreply.github.com>
2023-06-04 21:00:15 +0200

    selfHang

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Main  {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String userInput;
         String[] words = {"b","a","m","b","i"};
         String[] dashes = {"-","-","-","-","-"};
 
-        System.out.println("WELCOME TO THE HANGMAN");
+        System.out.println("WELCOME TO THE HANGMAN!!!");
         System.out.println(Arrays.toString(dashes));
 
-        int counter = 0;
-        while (! Arrays.equals(words,dashes)&& (counter<words.length+3)) {
+        int counter =0;
+        while (! Arrays.equals(words,dashes)&& counter<words.length+3) {
             System.out.println("Please guess a letter...");
             userInput = scan.nextLine();
 
@@ -25,9 +25,9 @@ public class Main {
             counter++;
         }
         if (Arrays.equals(words,dashes)){
-            System.out.println("YOU WIN!!!");
-        } else System.out.println("YOU HANG!!!!");
+            System.out.println("CONGRATULATIONS YOU WIN!!!!");
+        }else System.out.println("GAME OVER, YOU HANG......");
+
         scan.close();
     }
-
 }
